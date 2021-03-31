3e114d523b38f9e62823b3dbecf49c6f54e9b7cd
andrymoran <65819107+andrymoran@users.noreply.github.com>
2021-03-31 09:54:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
-      ? "Dijiste:" req.body.queryResult.parameters.echoText
+      ? "Dijiste:" + req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again."+req.body;
   return res.json({
 
