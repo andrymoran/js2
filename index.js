6820898c0f2e118d2f935295b71e9d87720c703a
andrymoran <65819107+andrymoran@users.noreply.github.com>
2021-03-31 09:58:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
-      ? "Dijiste:" + req.body.queryResult.parameters.echoText
+  
+  if(req.body.queryResult.parameters.echoText === "1234")
+     {
+       ? "Dijiste:" + req.body.queryResult.parameters.echoText
+       
+     }else(req.body.queryResult.parameters.echoText ==! "1234")
+     {
+       ? "Dijiste algo distinti a 1234 que fue:" + req.body.queryResult.parameters.echoText
+     }
+      
       : "Seems like some problem. Speak again."+req.body;
   return res.json({
 
